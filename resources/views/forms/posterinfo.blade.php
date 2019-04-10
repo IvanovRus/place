3522884d1105155acf9a9a66c878b05006dcc6dd
Your Name <you@example.com>
2019-04-10 21:29:55 +0400

    Изменено модальное окно информации о посте

--- resources/views/forms/posterinfo.blade.php
+++ resources/views/forms/posterinfo.blade.php
@@ -6,18 +6,18 @@
 
 <div class="form-group">
 	<label for="exampleTextarea">Сообщение</label>
-	{{ $message }}
+	<textarea class="form-control" name="msg" id="postersMsg" rows="3" disabled>{{ $message }}}</textarea>
 </div>
 <div class="form-group row">
 	<label for="example-text-input" class="col-xs-2 col-form-label">lat</label>
 	<div class="col-xs-10">
-		{{ $lat }}
+		<input class="form-control" name="lat" type="text" value="{{ $lat }}" id="postersLat" disabled>
 	</div>
 </div>
 <div class="form-group row">
 	<label for="example-text-input" class="col-xs-2 col-form-label">lng</label>
 	<div class="col-xs-10">
-		{{ $lon }}
+		<input class="form-control" name="lon" type="text" value="{{ $lon }}" id="postersLon" disabled>
 	</div>
 </div>
 
